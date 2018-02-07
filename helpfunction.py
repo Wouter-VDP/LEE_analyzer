16ee37f0de3478236db9b9645eec3ec1813a1792
Wouter Van De Pontseele <wvdp@fnal.gov>
2018-02-07 10:56:12 -0500

    Updated version

---
+++ helpfunction.py
@@ -0,0 +1,103 @@
+import numpy as np
+import pandas as pd
+
+
+# Function returns true if the point is within the volume specified by arr.
+def CheckBorderTPC(x,y,z,arr):
+    detectorx   =256.35     # In cm
+    detectory   =116.5      # Symmetric around 0     
+    detectorz   =1036.8
+    if (0+arr[0][0]) < x < (detectorx-arr[0][1]):
+            if (-detectory+arr[1][0])< y < (detectory-arr[1][1]):
+                    if (0+arr[2][0]) < z < (detectorz-arr[2][1]):
+                        return True
+    return False
+
+
+# Return true if the point is in the TPC with a tolerance.
+def CheckBorderFixed(x,y,z,tolerance=0):
+    arr = [[tolerance,tolerance],[tolerance,tolerance],[tolerance,tolerance]]
+    return CheckBorderTPC(x,y,z,arr)
+
+
+# Formatting
+def sciNot(x):
+    x=float(x)
+    return "{:.1f}".format(x)
+
+def safely_reduce_dtype(ser):  # pandas.Series or numpy.array  
+    orig_dtype = "".join([x for x in ser.dtype.name if x.isalpha()]) # float/int
+    mx = 2
+    for val in ser:
+        new_itemsize = np.min_scalar_type(val).itemsize
+        if mx < new_itemsize:
+            mx = new_itemsize
+    new_dtype = orig_dtype + str(mx * 8)
+    return ser.astype(new_dtype)
+
+
+def reduce_mem_usage(props):
+    start_mem_usg = props.memory_usage().sum() / 1024**2 
+    #print("Memory usage of properties dataframe is :",start_mem_usg," MB")
+    NAlist = [] # Keeps track of columns that have missing values filled in. 
+    for col in props.columns:
+        if props[col].dtype != object:  # Exclude strings
+            
+            # Print current column type
+            #print("******************************")
+            #print("Column: ",col)
+            #print("dtype before: ",props[col].dtype)
+            
+            # make variables for Int, max and min
+            IsInt = False
+            mx = props[col].max()
+            mn = props[col].min()
+            
+            # Integer does not support NA, therefore, NA needs to be filled
+            if not np.isfinite(props[col]).all(): 
+                NAlist.append(col)
+                props[col].fillna(mn-1,inplace=True)  
+                   
+            # test if column can be converted to an integer
+            asint = props[col].fillna(0).astype(np.int64)
+            result = (props[col] - asint)
+            result = result.sum()
+            if result > -0.01 and result < 0.01:
+                IsInt = True
+
+            
+            # Make Integer/unsigned Integer datatypes
+            if IsInt:
+                if mn >= 0:
+                    if mx < 255:
+                        props[col] = props[col].astype(np.uint8)
+                    elif mx < 65535:
+                        props[col] = props[col].astype(np.uint16)
+                    elif mx < 4294967295:
+                        props[col] = props[col].astype(np.uint32)
+                    else:
+                        props[col] = props[col].astype(np.uint64)
+                else:
+                    if mn > np.iinfo(np.int8).min and mx < np.iinfo(np.int8).max:
+                        props[col] = props[col].astype(np.int8)
+                    elif mn > np.iinfo(np.int16).min and mx < np.iinfo(np.int16).max:
+                        props[col] = props[col].astype(np.int16)
+                    elif mn > np.iinfo(np.int32).min and mx < np.iinfo(np.int32).max:
+                        props[col] = props[col].astype(np.int32)
+                    elif mn > np.iinfo(np.int64).min and mx < np.iinfo(np.int64).max:
+                        props[col] = props[col].astype(np.int64)    
+            
+            # Make float datatypes 32 bit
+            else:
+                props[col] = props[col].astype(np.float32)
+            
+            # Print new column type
+            #print("dtype after: ",props[col].dtype)
+            #print("******************************")
+    
+    # Print final result
+    #print("___MEMORY USAGE AFTER COMPLETION:___")
+    mem_usg = props.memory_usage().sum() / 1024**2 
+    #print("Memory usage is: ",mem_usg," MB")
+    #print("This is ",100*mem_usg/start_mem_usg,"% of the initial size")
+    return props, NAlist
